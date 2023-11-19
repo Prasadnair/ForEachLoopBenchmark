c5dc4928269c504180dbc95b584eeb7e04e992a7
Prasadnair <prasadnair44@gmail.com>
2023-11-18 22:32:49 -0500

    foreach benchmark

---
+++ ForEachAndListForEach/ForEachAndListForEach/ParallelVsListBenchmark.cs
@@ -0,0 +1,51 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace ForEachAndListForEach
+{
+    public class ParallelVsListBenchmark
+    {
+        private List<int> data;
+
+        [Params(1000000)] // Adjust the size of the dataset as needed
+        public int DataSize;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            // Generating a large collection of integers
+            Random rand = new Random();
+            data = new List<int>(DataSize);
+            for (int i = 0; i < DataSize; i++)
+            {
+                data.Add(rand.Next(100));
+            }
+        }
+
+        [Benchmark]
+        public void ParallelForEachBenchmark()
+        {
+            Parallel.ForEach(data, item =>
+            {
+                // Simulating a CPU-bound operation (e.g., some intensive calculation)
+                int result = Calculate(item);
+            });
+        }
+
+        [Benchmark]
+        public void ListForEachBenchmark()
+        {
+            data.ForEach(item =>
+            {
+                // Simulating the same CPU-bound operation
+                int result = Calculate(item);
+            });
+        }
+
+        // Simulated CPU-bound operation
+        private int Calculate(int value)
+        {
+            // Simulating some CPU-bound calculation
+            return value * value;
+        }
+    }
+}
---
+++ ForEachAndListForEach/ForEachAndListForEach/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Running;
+using ForEachAndListForEach;
+
+Console.WriteLine("Performance Comparison between Parallel.ForEach and List.ForEach");
+
+var summary = BenchmarkRunner.Run<ParallelVsListBenchmark>();
+
+Console.ReadLine();
+
